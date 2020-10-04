c1e8745322e2f2ed4cfb790296ef921e948d8b88
MuriloCicolin <mu_cicolin@hotmail.com>
2020-10-04 18:54:56 -0300

    Estrutura Inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Login/Login";
 
 function App() {
-  return <h1>Dogs</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
